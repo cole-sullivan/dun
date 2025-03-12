c3a3c7273a81452376febd741a08b6d03d33428c
cole-sullivan <mail@colesullivan.us>
2025-03-12 01:11:02 -0500

    Hello, World!

--- Example-iOS/Example-iOS/ViewController.swift
+++ Example-iOS/Example-iOS/ViewController.swift
@@ -12,9 +12,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Set view background to white (it's already white by default, but let's be explicit)
+        view.backgroundColor = .white
+        
+        // Create a label for the "Hello, World!" text
+        let helloLabel = UILabel()
+        helloLabel.text = "Hello, World!"
+        helloLabel.textColor = .black
+        helloLabel.font = UIFont.systemFont(ofSize: 24)
+        helloLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Add the label to the view
+        view.addSubview(helloLabel)
+        
+        // Center the label in the view
+        NSLayoutConstraint.activate([
+            helloLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            helloLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
-
-
 }
 
